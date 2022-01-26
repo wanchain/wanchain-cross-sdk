ec92f316a5ff88e3c6dd6eb2f3122d0aa1818720
zhwir <zhwir@163.com>
2022-01-26 16:13:36 +0800

    fix check Byron address

--- src/utils/tool.js
+++ src/utils/tool.js
@@ -126,20 +126,30 @@ function bytesAddressToBinary(bytes) {
 function isValidAdaAddress(address, network) {
   const networkId = (network === "testnet")? 0 : 1;
   try {
-    let addr = wasm.Address.from_bech32(address);
-    let prefix = bytesAddressToBinary(addr.to_bytes()).slice(0, 4);
-    if (parseInt(prefix, 2) > 7) {
-      return false;
-    }
+    let addr = wasm.ByronAddress.from_base58(address);
+    console.debug("%s is ADA Byron base58 address", address);
     return (addr.network_id() === networkId);
   } catch (e) {
-    console.debug("%s is not ADA bech32 address: %O", address, e);
+    console.debug("%s is not ADA Byron base58 address: %O", address, e);
   }
   try {
-    let addr = wasm.ByronAddress.from_base58(address);
-    return (addr.network_id() === networkId);
+    let addr = wasm.Address.from_bech32(address);
+    try {
+      let byronAddr = wasm.ByronAddress.from_address(addr);
+      if (byronAddr) {
+        console.debug("%s is ADA Byron bech32 address", address);
+      }
+      return (byronAddr.network_id() === networkId); // byronAddr is undefined to throw error
+    } catch (e) {
+      let prefix = bytesAddressToBinary(addr.to_bytes()).slice(0, 4);
+      console.log("%s is Shelly type %s address", address, prefix);
+      if (parseInt(prefix, 2) > 7) {
+        return false;
+      }
+      return (addr.network_id() === networkId);
+    }
   } catch (e) {
-    console.debug("%s is not ADA base58 address: %O", address, e);
+    console.debug("%s is not ADA bech32 address: %O", address, e);
   }
   return false;
 }
