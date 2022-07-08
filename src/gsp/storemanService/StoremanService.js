133c0d0bbfa0dd1d0ecd85bb20d3349911c8614c
zhwir <zhwir@163.com>
2022-07-08 16:28:54 +0800

    update getMinCrossChainAmount

--- src/gsp/storemanService/StoremanService.js
+++ src/gsp/storemanService/StoremanService.js
@@ -34,13 +34,14 @@ class StoremanService {
                 } else if (tokenPair.toAccount == 0) {
                     minAmountChain = tokenPair.toChainType;
                 }
+                let minAmountDecimals = (minAmountChain === tokenPair.fromChainType)? tokenPair.fromDecimals : tokenPair.toDecimals;
                 let [quota, min] = await Promise.all([
                     this.m_iwanBCConnector.getStoremanGroupQuota(fromChainType, storemanGroupId, [tokenPair.ancestorSymbol], toChainType),
                     this.m_iwanBCConnector.getMinCrossChainAmount(minAmountChain, tokenPair.ancestorSymbol)
                 ]);
                 // console.debug("getStroremanGroupQuotaInfo: %s, %s, %s, %s, %O", fromChainType, storemanGroupId, tokenPair.ancestorSymbol, toChainType, quota);
                 let maxQuota = new BigNumber(quota[0].maxQuota).div(Math.pow(10, parseInt(decimals)));
-                let minQuota = new BigNumber(min[tokenPair.ancestorSymbol]).div(Math.pow(10, parseInt(tokenPair.decimals))); // TODO: adapt to fromDecimals
+                let minQuota = new BigNumber(min[tokenPair.ancestorSymbol]).div(Math.pow(10, parseInt(minAmountDecimals)));
                 return {maxQuota: maxQuota.toFixed(), minQuota: minQuota.toFixed()};
             }            
         } catch (err) {
