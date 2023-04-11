c99e8742377e4cfad8c6a447ce1eab490289b07e
zhwir <zhwir@163.com>
2023-04-11 16:40:01 +0800

    remove debug

--- packages/cardano/src/wasm/index.js
+++ packages/cardano/src/wasm/index.js
@@ -5,12 +5,10 @@ let wasm = null;
 async function init() {
   let source = require("./cardano_serialization_lib_bg.wasm");
   const fetchPromise = fetch(source);
-  const imports = { "__wbindgen_placeholder__": Object.assign({}, wasmBg)};
-  const { instance } = await WebAssembly.instantiateStreaming(fetchPromise, imports);
-  console.log("wasm instance: %O", instance)
+  const imports = {"__wbindgen_placeholder__": Object.assign({}, wasmBg)};
+  const {instance} = await WebAssembly.instantiateStreaming(fetchPromise, imports);
   wasmBg.setWasm(instance.exports);
   wasm = wasmBg;
-  console.log("wasm instance.exports: %O", instance.exports);
 }
 
 function getWasm() {
