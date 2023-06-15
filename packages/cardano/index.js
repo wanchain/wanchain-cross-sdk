05c8c99846940e358dd7cebc9d407c8d20ea3e1b
zhwir <zhwir@163.com>
2023-06-15 20:47:56 +0800

    fix wasm

--- packages/cardano/index.js
+++ packages/cardano/index.js
@@ -4,12 +4,22 @@ const YoroiWallet = require("./src/wallet/yoroi");
 const Signer = require("./src/signer");
 const tool = require("./src/tool");
 const CoinSelection = require("./src/coinSelection");
+const sdkContractsMgr = require("cardano-contract-sdk/contracts-mgr.js");
+const sdkContracts = require("cardano-contract-sdk/contracts.js");
+const sdkOgmiosUtils = require("cardano-contract-sdk/ogmios-utils.js");
+const sdkSdk = require("cardano-contract-sdk/sdk.js");
+const sdkUtils = require("cardano-contract-sdk/utils.js");
 
 async function init() {
   await wasm.init();
   let _wasm = wasm.getWasm();
   tool.setWasm(_wasm);
   CoinSelection.setWasm(_wasm);
+  sdkContractsMgr.setWasm(_wasm);
+  sdkContracts.setWasm(_wasm);
+  sdkOgmiosUtils.setWasm(_wasm);
+  sdkSdk.setWasm(_wasm);
+  sdkUtils.setWasm(_wasm);
 }
 
 module.exports = {
