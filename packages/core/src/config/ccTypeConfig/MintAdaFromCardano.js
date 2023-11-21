bff8764a867935f071d0792830086a4d8d88b09b
zhwir <zhwir@163.com>
2023-11-21 14:26:13 +0800

    test code

--- packages/core/src/config/ccTypeConfig/MintAdaFromCardano.js
+++ packages/core/src/config/ccTypeConfig/MintAdaFromCardano.js
@@ -14,13 +14,15 @@ module.exports = class MintAdaFromCardano {
       let value = new BigNumber(convert.value).multipliedBy(Math.pow(10, tokenPair.fromDecimals)).toFixed(0);
       // fee is not necessary, storeman agent get fee from config contract
       let fee = tool.parseFee(convert.fee, convert.value, tokenPair.readableSymbol, {formatWithDecimals: false});
-      let networkFee = tool.parseFee(convert.fee, convert.value, "ADA", {formatWithDecimals: false, feeType: "networkFee"});
+      // let networkFee = tool.parseFee(convert.fee, convert.value, "ADA", {formatWithDecimals: false, feeType: "networkFee"});
+      let networkFee = new BigNumber(value).mod("3000000").toFixed(0);
+      let mockFeeHolder = new BigNumber(value).dividedToIntegerBy("1000000").mod("2").isZero();
       let toChainType = tokenPair.toChainType;
       let params = {
         ccTaskId: convert.ccTaskId,
         toChainType,
         crossScAddr: tokenPair.fromScInfo.crossScAddr,
-        feeHolder: tokenPair.fromScInfo.feeHolder,
+        feeHolder: mockFeeHolder? "addr_test1qr2h8sc5v5wg4eg0ennegxvpqrtdxnhxldgaysakvmh5tx4yqse03kx9yltqx3w4sgvrc23n75wuj4vtglj0aafecaqszc0l33" : tokenPair.fromScInfo.feeHolder,
         userAccount: tool.getStandardAddressInfo(toChainType, convert.toAddr, this.configService.getExtension(toChainType)).ascii,
         toAddr: convert.toAddr, // for readability
         storemanGroupId: convert.storemanGroupId,
