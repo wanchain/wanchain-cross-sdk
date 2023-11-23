832641fd2bf5abc5b4cc2e47920571be70ca126b
zhwir <zhwir@163.com>
2023-11-23 10:56:56 +0800

    remove cardano network fee test code

--- packages/core/src/config/ccTypeConfig/BurnFromCardano.js
+++ packages/core/src/config/ccTypeConfig/BurnFromCardano.js
@@ -14,15 +14,13 @@ module.exports = class BurnFromCardano {
       let value = new BigNumber(convert.value).multipliedBy(Math.pow(10, tokenPair.toDecimals)).toFixed(0);
       // fee is not necessary, storeman agent get fee from config contract
       let fee = tool.parseFee(convert.fee, convert.value, tokenPair.readableSymbol, {formatWithDecimals: false});
-      // let networkFee = tool.parseFee(convert.fee, convert.value, "ADA", {formatWithDecimals: false, feeType: "networkFee"});
-      let networkFee = new BigNumber(value).mod("3000000").toFixed(0);
-      let mockFeeHolder = new BigNumber(value).dividedToIntegerBy("1000000").mod("2").isZero();
+      let networkFee = tool.parseFee(convert.fee, convert.value, "ADA", {formatWithDecimals: false, feeType: "networkFee"});
       let toChainType = tokenPair.fromChainType;
       let params = {
         ccTaskId: convert.ccTaskId,
         toChainType,
         crossScAddr: tokenPair.toScInfo.crossScAddr,
-        feeHolder: mockFeeHolder? "addr_test1qr2h8sc5v5wg4eg0ennegxvpqrtdxnhxldgaysakvmh5tx4yqse03kx9yltqx3w4sgvrc23n75wuj4vtglj0aafecaqszc0l33" : tokenPair.toScInfo.feeHolder,
+        feeHolder: tokenPair.toScInfo.feeHolder,
         userAccount: tool.getStandardAddressInfo(toChainType, convert.toAddr, this.configService.getExtension(toChainType)).ascii,
         toAddr: convert.toAddr, // for readability
         storemanGroupId: convert.storemanGroupId,
