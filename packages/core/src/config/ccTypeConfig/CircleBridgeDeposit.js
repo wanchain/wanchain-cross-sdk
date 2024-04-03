87389d64ed1402cc31bf6dae00d089c1bf952bb8
zhwir <zhwir@163.com>
2024-04-03 19:14:45 +0800

    cctp sol to avax

--- packages/core/src/config/ccTypeConfig/CircleBridgeDeposit.js
+++ packages/core/src/config/ccTypeConfig/CircleBridgeDeposit.js
@@ -19,15 +19,23 @@ module.exports = class CircleBridgeDeposit extends TokenHandler {
   }
 
   async buildDeposit(steps, tokenPair, convert) {
-    let chainInfo = (convert.convertType === "MINT")? tokenPair.fromScInfo : tokenPair.toScInfo;
-    let decimals = (convert.convertType === "MINT")? tokenPair.fromDecimals : tokenPair.toDecimals;
-    let tokenAccount = (convert.convertType === "MINT")? tokenPair.fromAccount : tokenPair.toAccount;
-    let toChainType = (convert.convertType === "MINT")? tokenPair.toChainType : tokenPair.fromChainType;
+    let direction = (convert.convertType === "MINT");
+    let chainInfo = direction? tokenPair.fromScInfo : tokenPair.toScInfo;
+    let decimals = direction? tokenPair.fromDecimals : tokenPair.toDecimals;
+    let tokenAccount = direction? tokenPair.fromAccount : tokenPair.toAccount;
+    let toChainType = direction? tokenPair.toChainType : tokenPair.fromChainType;
     let value = new BigNumber(convert.value).multipliedBy(Math.pow(10, decimals));
     let unit = this.chainInfoService.getCoinSymbol(chainInfo.chainType);
     let networkFee = tool.parseFee(convert.fee, convert.value, unit, {formatWithDecimals: false});
     let operateFee = tool.parseFee(convert.fee, convert.value, tokenPair.readableSymbol, {formatWithDecimals: false});
-    let toAddressInfo = tool.getStandardAddressInfo(toChainType, convert.toAddr, this.configService.getExtension(toChainType));
+    let innerToAddr = convert.toAddr;
+    if (toChainType === "SOL") {
+      let sol = this.configService.getExtension(toChainType);
+      let toAccount = tool.ascii2letter(direction? tokenPair.toAccount : tokenPair.fromAccount);
+      innerToAddr = sol.tool.getAssociatedTokenAddressSync(sol.tool.getPublicKey(toAccount), sol.tool.getPublicKey(convert.toAddr)).toString();
+      console.log({innerToAddr});
+    }
+    let toAddressInfo = tool.getStandardAddressInfo(toChainType, innerToAddr, this.configService.getExtension(toChainType));
     let params = {
       ccTaskId: convert.ccTaskId,
       fromAddr: convert.fromAddr,
