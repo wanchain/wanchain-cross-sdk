86010e7cf901f697efe93cafc8506c47ce19cf25
unknown <zhwir@163.com>
2025-05-20 12:20:24 +0800

    rename Talos to AGENT

--- packages/core/src/gsp/tokenPairService/tokenPairService.js
+++ packages/core/src/gsp/tokenPairService/tokenPairService.js
@@ -455,7 +455,7 @@ class TokenPairService {
       } else if (symbol === "ELisforLiar") {
         return "LIAR";
       } else if (symbol === "Talos") {
-        return "AGENTIC";
+        return "AGENT";
       } else {
         return symbol;
       }
