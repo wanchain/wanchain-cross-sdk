7f929f44001bd08940ca0a2cd6e55ccab7b95bfb
unknown <zhwir@163.com>
2025-07-15 15:56:28 +0800

    fix sui token name

--- packages/core/src/bridge/wanBridge.js
+++ packages/core/src/bridge/wanBridge.js
@@ -441,11 +441,9 @@ class WanBridge extends EventEmitter {
         } else { // nft
           return policyId; // policyId
         }
-      } else if (chainType === "SOL") {
-        return tool.ascii2letter(tool.hexStrip0x(tokenAccount));
       } else if (chainType === "ALGO") {
         return Number(tokenAccount);
-      } else if (["ATOM", "NOBLE", "KAVA"].includes(chainType)) { // cosmos token account is ascii of name
+      } else if (["ATOM", "NOBLE", "KAVA", "SOL", "SUI"].includes(chainType)) { // ascii of name
         return tool.ascii2letter(tool.hexStrip0x(tokenAccount));
       } else {
         return tool.getStandardAddressInfo(chainType, tokenAccount, this.configService.getExtension(chainType)).native;
