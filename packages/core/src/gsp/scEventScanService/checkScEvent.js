1985a04ff97092c171ca9fd570303c9c4d057fd0
unknown <zhwir@163.com>
2025-08-15 14:55:22 +0800

    update evm scan block range

--- packages/core/src/gsp/scEventScanService/checkScEvent.js
+++ packages/core/src/gsp/scEventScanService/checkScEvent.js
@@ -7,7 +7,8 @@ const DefaultScanBatchSize = 1000;
 const CustomizedScanBatchSize = {
   SGB: 30,
   OKT: 300,
-  OKB: 100
+  OKB: 100,
+  MATIC: 100
 };
 
 const EvmEventTypes = ["MINT", "BURN", "MINTNFT", "BURNNFT", "circleMINT", "cctpV2MINT"];
