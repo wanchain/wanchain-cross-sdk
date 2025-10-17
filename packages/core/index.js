5511448de58d1f90c873d5dc54905b8dbfc35937
Clarence <jiangduoduo@jiangxinglin.local>
2025-10-17 17:54:42 +0800

    change extension

--- packages/core/index.js
+++ packages/core/index.js
@@ -1,11 +1,5 @@
 const WanBridge = require('./src/bridge/wanBridge');
-const Web3Wallet = require('./src/wallet/web3Wallet');
-const UnisatWallet = require('./src/wallet/unisat');
-const OkxBitcoinWallet = require('./src/wallet/okxBitcoin');
 
 module.exports = {
-  WanBridge,
-  Web3Wallet,
-  UnisatWallet,
-  OkxBitcoinWallet
+  WanBridge
 };
