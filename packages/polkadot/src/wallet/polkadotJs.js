2ca4a609747094a0cd8500c89ccabddec303c5c6
unknown <zhwir@163.com>
2025-11-12 14:54:02 +0800

    update polkadot mainnet rpc

--- packages/polkadot/src/wallet/polkadotJs.js
+++ packages/polkadot/src/wallet/polkadotJs.js
@@ -5,7 +5,7 @@ const BigNumber = require('bignumber.js');
 
 const DefaultProvider = {
   Polkadot: {
-    mainnet: "wss://rpc.polkadot.io",
+    mainnet: "wss://polkadot-asset-hub-rpc.polkadot.io",
     testnet: "wss://westend-asset-hub-rpc.polkadot.io"
   },
   Phala: {
