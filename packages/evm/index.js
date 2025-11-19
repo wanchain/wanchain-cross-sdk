7d337049bf8209defd17cc1b492adae788d15fc4
Clarence <jiangduoduo@jiangxinglin.local>
2025-11-19 18:39:45 +0800

    del xdc wallet

--- packages/evm/index.js
+++ packages/evm/index.js
@@ -1,7 +1,6 @@
 const MetamaskWallet = require("./src/wallet/metamask");
 const RabbyWallet = require("./src/wallet/rabby");
 const OKXWallet = require("./src/wallet/okx");
-const XDCWallet = require("./src/wallet/xdc");
 const CtrlWallet = require("./src/wallet/ctrl");
 const WalletConnect = require("./src/wallet/walletconnect");
 const WanWallet = require("./src/wallet/wanwallet");
@@ -10,7 +9,6 @@ module.exports = {
   MetamaskWallet,
   RabbyWallet,
   OKXWallet,
-  XDCWallet,
   CtrlWallet,
   WalletConnect,
   WanWallet
