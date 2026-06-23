81ef356aca2f841266d02f65d2607f1f6d8eca8b
unknown <zhwir@163.com>
2026-06-23 14:36:23 +0800

    midnight support testnet

--- packages/core/src/bridge/wanBridge.js
+++ packages/core/src/bridge/wanBridge.js
@@ -35,7 +35,7 @@ class WanBridge extends EventEmitter {
   }
 
   async init(iwanAuth, options = {}) {
-    console.debug("SDK: init, network: %s, isTestMode: %s, smgName: %s, prefer: %s, ver: 2606171845", this.network, this.isTestMode, this.smgName, this.prefer);
+    console.debug("SDK: init, network: %s, isTestMode: %s, smgName: %s, prefer: %s, ver: 2606231435", this.network, this.isTestMode, this.smgName, this.prefer);
     try {
       this._service = new StartService();
       await this._service.init(this.network, this.stores, iwanAuth, Object.assign(options, { isTestMode: this.isTestMode, prefer: this.prefer }));
@@ -129,7 +129,7 @@ class WanBridge extends EventEmitter {
     console.debug("SDK: createTask at %s ms, assetType: %s, fromChainName: %s, toChainName: %s, amount: %O, fromAccount: %s, toAccount: %s, options: %O",
       tool.getCurTimestamp(), assetType, fromChainName, toChainName, amount, fromAccount, toAccount, this._getDebugOptions(options));
     if ((this.network === "testnet") && (this.smgName.indexOf("dev") !== 0)) {
-      let devChains = ["Cosmos", "Kava", "Noble", "Midnight"];
+      let devChains = ["Cosmos", "Kava", "Noble"];
       if (devChains.includes(fromChainName) || devChains.includes(toChainName)) {
         throw new Error("Only support dev group");
       }
